f746e96a9748ad59a59d5ff20a17d936b7f29cbb
Davis Lin <da860610@gmail.com>
2018-01-02 01:15:37 +0800

    Fix script

--- initial.sh
+++ initial.sh
@@ -1,14 +1,11 @@
-echo "alias ls='ls --color=always'" >> /etc/bashrc
-tail -n 1 /etc/bashrc
-
-if [ $(whoami)=='root' ]; then
-    # For root
-    echo 'PS1="\e[0;31m\u\e[m \e[1;30min\e[m \e[0;32m\H\e[m \e[1;30mat\e[m \e[1;36m\w\e[m \e[0;35m[\t]\e[m\n\$ "' >> ~/.bashrc
-    tail -n 1 /root/.bashrc
-else
-    # For current
-    echo 'PS1="\e[0;34m\u\e[m \e[1;30min\e[m \e[0;32m\H\e[m \e[1;30mat\e[m \e[1;36m\w\e[m \e[0;35m[\t]\e[m\n\$ "' >> ~/.bashrc
-    tail -n 1 ~/.bashrc
+if [ ! -f ./bashrc ]; then
+    echo "The bashrc file not exist!"
+    exit
 fi
 
-source ~/.bashrc
+echo "Backup the ~/.bashrc to ~/.bashrc.bk"
+mv ~/.bashrc ~/.bashrc.$(date +%Y%m%d-%H%M%S)
+echo "Copy the new .bashrc to ~/.bashrc"
+cp -a ./bashrc ~/.bashrc
+
+ls -al ~ | grep ".bashrc"
