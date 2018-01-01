4173249f40339476c7a3651b28d3a49d5e3933d6
Davis Lin <da860610@gmail.com>
2018-01-01 22:34:21 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -3,7 +3,7 @@ tail -n 1 /etc/bashrc
 
 if [ $(whoami)=='root' ]; then
     # For root
-    echo 'PS1="\e[0;31m\u\e[m \e[1;30min\e[m \e[0;32m\H\e[m \e[1;30mat\e[m \e[1;36m\w\e[m \e[0;35m[\t]\e[m\n\$ "' >> /root/.bashrc
+    echo 'PS1="\e[0;31m\u\e[m \e[1;30min\e[m \e[0;32m\H\e[m \e[1;30mat\e[m \e[1;36m\w\e[m \e[0;35m[\t]\e[m\n\$ "' >> ~/.bashrc
     tail -n 1 /root/.bashrc
 else
     # For current
@@ -11,3 +11,4 @@ else
     tail -n 1 ~/.bashrc
 fi
 
+source ~/.bashrc
